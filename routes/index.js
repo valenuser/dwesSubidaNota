2dc0d378c3068d73712fff95d808fc45c3dc77c5
Valentin <vpavonlopez@gmail.com>
2024-03-03 20:13:17 +0100

    diseño login y register hechos

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,11 @@ const router = express.Router()
 router.get('/',(req,res)=>{
     res.render('index')
 })
+router.get('/login',(req,res)=>{
+    res.render('login')
+})
+router.get('/register',(req,res)=>{
+    res.render('register')
+})
 
 module.exports = router
